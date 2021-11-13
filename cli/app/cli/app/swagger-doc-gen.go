7aa431b479aad2374c40c154e206d61f026032ff
hz <hedzrz@gmail.com>
2021-11-13 10:00:16 +0800

    no way to embed swagger without missed go.sum error

--- cli/app/cli/app/swagger-doc-gen.go
+++ cli/app/cli/app/swagger-doc-gen.go
@@ -2,11 +2,11 @@ package main
 
 // Uncomment the following codes if you wanna generating swagger doc automatically
 
-//go:generate swag init --output ./swaggerdocs
-
-import (
-	_ "cmdr-starter/cli/app/cli/app/swaggerdocs"
-
-	_ "github.com/alecthomas/template"
-	_ "github.com/swaggo/swag"
-)
+////go:generate swag init --output ./swaggerdocs
+//
+//import (
+//	_ "cmdr-starter/cli/app/cli/app/swaggerdocs"
+//
+//	_ "github.com/alecthomas/template"
+//	_ "github.com/swaggo/swag"
+//)
