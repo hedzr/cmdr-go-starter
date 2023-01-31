ceb3f606aebb8ad40f59c46f82cefb91f77c291e
hz <hedzrz@gmail.com>
2023-01-31 10:20:43 +0800

    update codes
    Signed-off-by: hz <hedzrz@gmail.com>

--- cli/your-starter/cmd/doc.go
+++ cli/your-starter/cmd/doc.go
@@ -3,7 +3,7 @@ package cmd
 //goland:noinspection GoNameStartsWithPackageName
 const (
 	appName   = "your-starter"
-	version   = "0.2.7"
+	version   = "1.0.0"
 	copyright = "your-starter - A devops tool - cmdr series"
 	desc      = "your-starter is an effective devops tool. It make an demo application for 'cmdr'"
 	longDesc  = `your-starter is an effective devops tool. It make an demo application for 'cmdr'.
--- cli/your-starter/main.go
+++ cli/your-starter/main.go
@@ -4,9 +4,9 @@ import (
 	cmd "cmdr-starter/cli/your-starter/cmd"
 )
 
-func init() {
-	// build.New(build.NewLoggerConfigWith(true, "logrus", "debug"))
-}
+// func init() {
+// 	// build.New(build.NewLoggerConfigWith(true, "logrus", "debug"))
+// }
 
 func main() {
 	cmd.Entry()
