309be2db6d6b8bb2721c08948c8c8aac98996aee
hz <hedzrz@gmail.com>
2025-04-15 11:19:22 +0800

    add jump cmd for dyncommand feature

--- cli/your-starter/cmd/all.go
+++ cli/your-starter/cmd/all.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/hedzr/cmdr/v2/cli"
+	"github.com/hedzr/cmdr/v2/examples/cmd"
 )
 
-var Commands = // append(
-[]cli.CmdAdder{
-	sndx{},
-} //,
-// cmd.Commands...,
-// )
+var Commands = append(
+	[]cli.CmdAdder{
+		sndx{},
+	},
+	cmd.Commands[0], // include only jumpCmd
+)
