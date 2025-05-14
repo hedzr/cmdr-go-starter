21f106ef5bd02ceb38ac6abf8ea075562d496768
hz <hedzrz@gmail.com>
2025-05-14 09:58:56 +0800

    bump to v2.1.17

--- doc.go
+++ doc.go
@@ -4,3 +4,9 @@ package starter
 //
 // For CI building please check out: cli/app/doc.go and cli/app/cli/app/main.go ...
 //
+
+const AppNameExample = "cmdr-go-starter" // appName for the current demo app
+
+const appName = "cmdr-go-starter" // appName of hedzr/cmdr-loaders package
+const version = "v2.1.17"         // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
+const Version = version
